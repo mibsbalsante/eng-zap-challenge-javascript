9d41378719cf1d1cd52e9a883281bc5618abc2c6
Mibs Balsante <mibsbalsante@gmail.com>
2021-07-04 12:49:14 -0400

    build: loads main js and html template

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,29 @@ const path = require('path')
 
 module.exports = {
     entry: path.resolve(__dirname, './src/index.js'),
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: {
+                  loader: 'html-loader',
+                },
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: { loader: 'babel-loader' },
+            }
+        ]
+    },
     output: {
         path: path.resolve(__dirname, './dist'),
         filename: 'index__[contenthash].js',
     },
     plugins: [
-        new HtmlWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './public/index.html'),
+            filename: './index.html',
+        }),
     ]
 }
