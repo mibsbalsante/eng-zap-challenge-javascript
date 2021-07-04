83ecc3e286fb2cb195e60c8107f72606e37a3995
Mibs Balsante <mibsbalsante@gmail.com>
2021-07-04 14:16:03 -0400

    build: webpack config for css with postcss

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,62 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const HTMLWpPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const path = require('path')
 
-module.exports = {
-    entry: path.resolve(__dirname, './src/index.js'),
-    module: {
-        rules: [
-            {
-                test: /\.html$/,
-                use: {
-                  loader: 'html-loader',
+module.exports = (env, arg) => {
+    const devMode = arg.mode !== "production";
+
+    return {
+        devtool: devMode ? 'eval' : 'source-map',
+        entry: path.resolve(__dirname, './src/index.js'),
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    exclude: /node_modules/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                importLoaders: 1,
+                                modules: {
+                                    localIdentName: devMode ? '[local]' : '[local]__[hash:base64:5]',
+                                },
+                            },
+                        },
+                        'postcss-loader',
+                    ],
                 },
-            },
-            {
-                test: /\.js$/,
-                exclude: /(node_modules)/,
-                use: { loader: 'babel-loader' },
-            }
-        ]
-    },
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: 'index__[contenthash].js',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, './public/index.html'),
-            filename: './index.html',
-        }),
-    ]
+                {
+                    test: /\.css$/,
+                    include: /node_modules/,
+                    exclude: /src/,
+                    use: ['css-loader', 'postcss-loader'],
+                },
+                {
+                    test: /\.html$/,
+                    use: {
+                      loader: 'html-loader',
+                    },
+                },
+                {
+                    test: /\.js$/,
+                    exclude: /(node_modules)/,
+                    use: { loader: 'babel-loader' },
+                }
+            ]
+        },
+        output: {
+            path: path.resolve(__dirname, './dist'),
+            filename: 'index__[contenthash].js',
+        },
+        plugins: [
+            new MiniCssExtractPlugin(),
+            new HtmlWebpackPlugin({
+                template: path.resolve(__dirname, './public/index.html'),
+                filename: './index.html',
+            }),
+        ],
+    }
 }
