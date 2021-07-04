8b8543607a04e605f46844e450bb05f6176ec2ad
Mibs Balsante <mibsbalsante@gmail.com>
2021-07-04 12:49:24 -0400

    build: react first component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.info('>>initial build')
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = (
+    <p>React first build</p>
+)
+
+ReactDOM.render(App, document.querySelector('#root'))
