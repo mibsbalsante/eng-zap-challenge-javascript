bbce8fbf576cd6ab2b9f490b123c7dd3d460555a
Mibs Balsante <mibsbalsante@gmail.com>
2021-07-06 19:39:41 -0400

    feat: request json file that will  fill the page

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,20 +1,30 @@
-import React from 'react'
+import React, { useCallback, useEffect } from 'react'
 import PropTypes from 'prop-types'
 
 import Feed from '@comp/feed'
 import Filters from '@comp/filters'
 import Pagination from '@comp/pagination'
+import request from '@util/request'
 
 import styles from './styles.css'
 
-const Home = ({ location }) => (
-  <div className={styles.pageHome}>
-    <p>Homepage: {location.pathname}</p>
-    <Filters />
-    <Feed />
-    <Pagination />
-  </div>
-)
+const Home = ({ location }) => {
+  const getUsers = useCallback(async () => {
+    const users = await request()
+    console.info('>>users', users)
+  }, [])
+
+  useEffect(getUsers, [])
+
+  return (
+    <div className={styles.pageHome}>
+      <p>Homepage: {location.pathname}</p>
+      <Filters />
+      <Feed />
+      <Pagination />
+    </div>
+  )
+}
 
 Home.propTypes = {
   location: PropTypes.shape({ pathname: PropTypes.string }).isRequired,
