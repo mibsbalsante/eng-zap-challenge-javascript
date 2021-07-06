a65cb98b4f2a167f6237dbe65a0200b329680423
Mibs Balsante <mibsbalsante@gmail.com>
2021-07-06 19:24:02 -0400

    feat: replace slider lib to remove jquery dep and add thumbnails

--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -1,56 +1,71 @@
 import React from 'react'
-import Slick from 'react-slick'
+import Carousel, {
+  Dots,
+  slidesToShowPlugin,
+  slidesToScrollPlugin,
+} from '@brainhubeu/react-carousel'
+import '@brainhubeu/react-carousel/lib/style.css'
 
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
 import styles from './styles.css'
+
+const slides = [
+  <img
+    key={600}
+    onClick={({ target }) => console.info('>>click', target)}
+    src='https://picsum.photos/600/400'
+  />,
+  <img
+    key={601}
+    onClick={({ target }) => console.info('>>click', target)}
+    src='https://picsum.photos/601/400'
+  />,
+  <img
+    key={602}
+    onClick={({ target }) => console.info('>>click', target)}
+    src='https://picsum.photos/602/400'
+  />,
+  <img
+    key={603}
+    onClick={({ target }) => console.info('>>click', target)}
+    src='https://picsum.photos/603/400'
+  />,
+  <img
+    key={604}
+    onClick={({ target }) => console.info('>>click', target)}
+    src='https://picsum.photos/604/400'
+  />,
+  <img
+    key={605}
+    onClick={({ target }) => console.info('>>click', target)}
+    src='https://picsum.photos/605/400'
+  />,
+]
 
 const Slider = () => (
   <div className={styles.slider}>
-    <Slick
-      {...{
-        adaptiveHeight: true,
-        centerPadding: '10px',
-        dots: false,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        swipeToSlide: true,
-        variableWidth: true,
-      }}
-    >
-      <div>
-        <img src='https://picsum.photos/300/200' />
-      </div>
-      <div>
-        <img src='https://picsum.photos/301/200' />
-      </div>
-      <div>
-        <img src='https://picsum.photos/302/200' />
-      </div>
-      <div>
-        <img src='https://picsum.photos/303/200' />
-      </div>
-      <div>
-        <img src='https://picsum.photos/304/200' />
-      </div>
-      <div>
-        <img src='https://picsum.photos/305/200' />
-      </div>
-      <div>
-        <img src='https://picsum.photos/306/200' />
-      </div>
-      <div>
-        <img src='https://picsum.photos/307/200' />
-      </div>
-      <div>
-        <img src='https://picsum.photos/308/200' />
-      </div>
-      <div>
-        <img src='https://picsum.photos/309/200' />
-      </div>
-    </Slick>
+    <Carousel
+      animationSpeed={300}
+      itemWidth={600}
+      plugins={[
+        'arrows',
+        'infinite',
+        'fastSwipe',
+        {
+          resolve: slidesToShowPlugin,
+          options: {
+            numberOfSlides: 2,
+          },
+        },
+        {
+          resolve: slidesToScrollPlugin,
+          options: {
+            numberOfSlides: 2,
+          },
+        },
+      ]}
+      slides={slides}
+    />
+    <Dots number={slides.length} thumbnails={slides} value={1} />
   </div>
 )
 
