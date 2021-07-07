e4c0c4cbe4d65a0cd1929a3bff05013718122590
Mibs Balsante <mibsbalsante@gmail.com>
2021-07-07 19:03:59 -0400

    feat: search component

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -4,9 +4,9 @@ import styles from './styles.css'
 
 const Search = () => (
   <form action='#' className={styles.search}>
-    <input type='text' placeholder='Pesquisar' />
-    <button>
-      <img src='#' alt='Enviar' />
+    <input type='text' placeholder='Pesquisar por localização' className={styles.input} />
+    <button type='submit' title='Enviar' className={styles.button}>
+      <i className='fas fa-search fa-lg'></i>
     </button>
   </form>
 )
