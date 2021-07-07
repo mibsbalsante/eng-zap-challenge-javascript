8ab790efcdcac422218f3ffa20684c81ace9614c
Mibs Balsante <mibsbalsante@gmail.com>
2021-07-07 16:06:02 -0400

    feat: navbar template and desktop styles

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,18 +1,42 @@
 import React from 'react'
+import classNames from 'classnames'
 import { Link } from 'react-router-dom'
 
 import Search from '@comp/search'
+import logoGrupo from '@img/grupozap.png'
+import logoVivaReal from '@img/vivareal.png'
+import logoZap from '@img/zap.png'
 
 import styles from './styles.css'
 
 const Navbar = () => (
   <header className={styles.navbar}>
-    <Link to='/'>Home</Link>
-    <nav>
-      <Link to='/zap'>Zap</Link>
-      <Link to='/vivareal'>VivaReal</Link>
-    </nav>
-    <Search />
+    <div className={styles.container}>
+      <Link
+        to='/'
+        alt='Mostrar todos os resultados'
+        className={classNames(styles.link, styles.linkHome)}
+      >
+        <img src={logoGrupo} alt='' />
+      </Link>
+      <nav className={styles.navigation}>
+        <Link
+          to='/vivareal'
+          alt='Mostrar apenas resultados de Viva Real'
+          className={classNames(styles.link, styles.linkVivareal)}
+        >
+          <img src={logoVivaReal} alt='' />
+        </Link>
+        <Link
+          to='/zap'
+          alt='Mostrar apenas resultados de Zap Imóveis'
+          className={classNames(styles.link, styles.linkZap)}
+        >
+          <img src={logoZap} alt='' />
+        </Link>
+      </nav>
+      <Search />
+    </div>
   </header>
 )
 
