c9b5e3b252d1139acbe017e51e7de353143e15c0
Mibs Balsante <mibsbalsante@gmail.com>
2021-07-08 16:09:14 -0400

    feat: fill cards with apartments data

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,35 +1,75 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 
 import styles from './styles.css'
 
-const Card = () => (
+const Card = ({
+  id,
+  address,
+  images,
+  bathrooms,
+  bedrooms,
+  parkingSpaces,
+  usableAreas,
+  pricingInfos,
+}) => (
   <article className={styles.card}>
-    <Link to='/imovel'>
-      <img src='#' alt='' />
+    <Link to={`/imovel/${id}`}>
+      <img src={images[0]} alt='' />
       <div>
-        <p>endr</p>
+        {address.geoLocation.precision !== 'NO_GEOCODE' ? (
+          <p>
+            {address.neighborhood}, {address.city}
+          </p>
+        ) : (
+          <p>Pergunte o endereço</p>
+        )}
         <p>
-          <span>R$ 2000,00</span>
-          <span>R$ 200,00</span>
+          {pricingInfos.businessType === 'SALE' ? (
+            <span>Preço: R$ {pricingInfos.price}</span>
+          ) : (
+            <span>Aluguel: R$ {pricingInfos.rentalTotalPrice}</span>
+          )}
         </p>
         <div>
           <p>
-            <span>25m²</span>
+            <span>{usableAreas}m²</span>
           </p>
           <p>
-            <span>2q</span>
+            <span>{bedrooms}q</span>
           </p>
           <p>
-            <span>2b</span>
+            <span>{bathrooms}b</span>
           </p>
           <p>
-            <span>2v</span>
+            <span>{parkingSpaces}v</span>
           </p>
         </div>
       </div>
     </Link>
   </article>
 )
+
+Card.propTypes = {
+  id: PropTypes.string.isRequired,
+  address: PropTypes.shape({
+    geoLocation: PropTypes.shape({
+      precision: PropTypes.string,
+    }),
+    neighborhood: PropTypes.string,
+    city: PropTypes.string,
+  }).isRequired,
+  images: PropTypes.arrayOf(PropTypes.string).isRequired,
+  bathrooms: PropTypes.number.isRequired,
+  bedrooms: PropTypes.number.isRequired,
+  parkingSpaces: PropTypes.number.isRequired,
+  usableAreas: PropTypes.number.isRequired,
+  pricingInfos: PropTypes.shape({
+    businessType: PropTypes.string,
+    price: PropTypes.string,
+    rentalTotalPrice: PropTypes.string,
+  }).isRequired,
+}
 
 export default Card
