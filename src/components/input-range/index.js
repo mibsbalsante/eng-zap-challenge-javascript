411451e505c6610ad01aedd367ba25325ebee15f
Mibs Balsante <mibsbalsante@gmail.com>
2021-07-08 14:49:34 -0400

    refactor: input range to receive props into its child component

--- src/components/input-range/index.js
+++ src/components/input-range/index.js
@@ -1,11 +1,38 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+
+import helperStyles from '@config/helper-classes.css'
 
 import styles from './styles.css'
 
-const InputRange = () => (
-  <div className={styles.inputRange}>
-    <input type='number' placeholder='Mínimo' />
-    <input type='number' placeholder='Máximo' />
+const Input = ({ format, placeholder }) => (
+  <div className={styles.inputContainer}>
+    <input
+      type='number'
+      min={0}
+      placeholder={placeholder}
+      className={classNames(helperStyles.formControl, styles.input, {
+        [styles.money]: format === 'money',
+      })}
+    />
+    {format === 'money' && <span className={styles.currency}>R$</span>}
+  </div>
+)
+
+Input.propTypes = {
+  format: PropTypes.string,
+  placeholder: PropTypes.string.isRequired,
+}
+
+Input.defaultProps = {
+  format: 'int',
+}
+
+const InputRange = props => (
+  <div className={styles.range}>
+    <Input {...props} placeholder='Mínimo' />
+    <Input {...props} placeholder='Máximo' />
   </div>
 )
 
