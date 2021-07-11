5883ca0ba11a0e13b18fe189ecc0cef3eccc33a9
Mibs Balsante <mibsbalsante@gmail.com>
2021-07-11 00:20:32 -0400

    feat: use currency input with input range component

--- src/components/input-range/index.js
+++ src/components/input-range/index.js
@@ -1,49 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
-import classNames from 'classnames'
-import CurrencyInput from 'react-currency-input-field'
 
-import helperStyles from '@config/helper-classes.css'
+import { useContext } from '@context/apartments'
 
+import Input from './input'
 import styles from './styles.css'
 
-const Input = ({ field, format, placeholder, type }) => (
-  <div className={styles.inputContainer}>
-    <CurrencyInput
-      id={`${field}-${type}`}
-      name={`${field}-${type}`}
-      allowDecimals={format === 'money'}
-      allowNegativeValue={false}
-      placeholder={placeholder}
-      decimalSeparator=','
-      groupSeparator='.'
-      decimalsLimit={2}
-      onValueChange={(value, name) => console.info('>>inputrange values::', value, name)}
-      className={classNames(helperStyles.formControl, styles.input, {
-        [styles.money]: format === 'money',
-      })}
-    />
+const InputRange = props => {
+  const { state, dispatch } = useContext()
 
-    {format === 'money' && <span className={styles.currency}>R$</span>}
-  </div>
-)
+  const [fieldValues, setFieldValues] = useState({ min: 0, max: 0 })
 
-Input.propTypes = {
-  format: PropTypes.string,
+  const handleReplace = ({ value, type }) => {
+    let current = Number((value || '').replace(/\./g, '').replace(/,/g, '.'))
+
+    setFieldValues(old => ({ ...old, ...{ [type]: current } }))
+    dispatch({ type: 'SET_RANGE_FIELD', payload: { field: props.field, value: fieldValues } })
+  }
+
+  useEffect(() => {
+    setFieldValues(state[props.field])
+  }, [])
+
+  return (
+    <div className={styles.range}>
+      <Input
+        {...props}
+        type='min'
+        placeholder='Mínimo'
+        value={fieldValues.min}
+        onChange={handleReplace}
+      />
+      {/* TODO: set max input min value to min input value */}
+      <Input
+        {...props}
+        type='max'
+        placeholder='Máximo'
+        value={fieldValues.max}
+        onChange={handleReplace}
+      />
+    </div>
+  )
+}
+
+InputRange.propTypes = {
   field: PropTypes.string.isRequired,
-  placeholder: PropTypes.string.isRequired,
-  type: PropTypes.string.isRequired,
 }
-
-Input.defaultProps = {
-  format: 'int',
-}
-
-const InputRange = props => (
-  <div className={styles.range}>
-    <Input {...props} type='min' placeholder='Mínimo' />
-    <Input {...props} type='max' placeholder='Máximo' />
-  </div>
-)
 
 export default InputRange
