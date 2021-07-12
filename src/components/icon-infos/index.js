fd2fffba6ebdf94c0b42fff51dc59920f1cd805f
Mibs Balsante <mibsbalsante@gmail.com>
2021-07-11 20:13:31 -0400

    feat: classname prop to customize icon infos

--- src/components/icon-infos/index.js
+++ src/components/icon-infos/index.js
@@ -16,8 +16,8 @@ Info.propTypes = {
   text: PropTypes.string.isRequired,
 }
 
-const IconInfos = ({ bedrooms, bathrooms, parkingSpaces, usableAreas }) => (
-  <div className={styles.infos}>
+const IconInfos = ({ className, bedrooms, bathrooms, parkingSpaces, usableAreas }) => (
+  <div className={classNames(styles.infos, className)}>
     <Info icon='fa-home' text={`${usableAreas}m²`} />
     <Info icon='fa-bed' text={`${bedrooms} quarto${bedrooms !== 1 ? 's' : ''}`} />
     <Info icon='fa-bath' text={`${bathrooms} banheiro${bathrooms !== 1 ? 's' : ''}`} />
@@ -26,10 +26,15 @@ const IconInfos = ({ bedrooms, bathrooms, parkingSpaces, usableAreas }) => (
 )
 
 IconInfos.propTypes = {
+  className: PropTypes.string,
   bathrooms: PropTypes.number.isRequired,
   bedrooms: PropTypes.number.isRequired,
   parkingSpaces: PropTypes.number.isRequired,
   usableAreas: PropTypes.number.isRequired,
+}
+
+IconInfos.defaultProps = {
+  className: undefined,
 }
 
 export default IconInfos
