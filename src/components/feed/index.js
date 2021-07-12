308588392602eb3a839f6f17fb225f5d0210aabe
Mibs Balsante <mibsbalsante@gmail.com>
2021-07-12 16:14:53 -0400

    feat: show not found component when feed doesnt return results

--- src/components/feed/index.js
+++ src/components/feed/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import classNames from 'classnames'
 
 import { ApartmentsConsumer } from '@context/apartments'
+import NotFound from '@comp/not-found'
 import Card from '@comp/card'
 
 import styles from './styles.css'
@@ -10,7 +11,13 @@ import styles from './styles.css'
 const Feed = ({ className }) => (
   <div className={classNames(styles.feed, className)}>
     <ApartmentsConsumer>
-      {({ state }) => state.results.map(result => <Card key={result.id} {...result} />)}
+      {({ state }) =>
+        state.results && state.results.length > 0 ? (
+          state.results.map(result => <Card key={result.id} {...result} />)
+        ) : (
+          <NotFound />
+        )
+      }
     </ApartmentsConsumer>
   </div>
 )
