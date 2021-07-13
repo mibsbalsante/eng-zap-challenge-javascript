b9ef3a312d400509950fa80c467fe67588b8a88d
Mibs Balsante <mibsbalsante@gmail.com>
2021-07-12 21:24:40 -0400

    refactor: move all home dispatches and state handlers to a hoc

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
 import { useContext } from '@context/apartments'
@@ -8,60 +8,12 @@ import Filters from '@comp/filters'
 import Loading from '@comp/loading'
 import NotFound from '@comp/not-found'
 import Pagination from '@comp/pagination'
+import withFilters from '@hoc/filters'
 
 import styles from './styles.css'
 
-const Home = ({ location }) => {
-  const { state, dispatch } = useContext()
-
-  useEffect(() => {
-    if (state.results.length > 0) {
-      setTimeout(() => dispatch({ type: 'SET_LOADING', payload: false }), 600)
-    }
-  }, [state.results])
-
-  useEffect(() => {
-    // fix stuck on loading
-    setTimeout(() => {
-      dispatch({ type: 'SET_LOADING', payload: false })
-    }, 5000)
-  }, [state.isLoading])
-
-  // TODO: move all searchparams logic to a hoc
-
-  useEffect(() => {
-    dispatch({ type: 'SET_LOADING', payload: true })
-
-    const query = new URLSearchParams(location.search)
-    const params = Object.fromEntries(query.entries())
-    const parseToNumber = val => Number(val) || 0
-    const rangeParams = {
-      priceRange: {
-        field: 'priceRange',
-        value: { min: parseToNumber(params.priceMin), max: parseToNumber(params.priceMax) },
-      },
-      squareMetersRange: {
-        field: 'squareMetersRange',
-        value: { min: parseToNumber(params.areasMin), max: parseToNumber(params.areasMax) },
-      },
-    }
-
-    dispatch({ type: 'SET_FILTERS', payload: params })
-    dispatch({ type: 'SET_RANGE_FIELD', payload: rangeParams.priceRange })
-    dispatch({
-      type: 'SET_RANGE_FIELD',
-      payload: rangeParams.squareMetersRange,
-    })
-    dispatch({ type: 'SET_FILTERED_RESULTS' })
-  }, [location.search, state.apartments])
-
-  useEffect(() => {
-    if (location.pathname !== '/')
-      dispatch({ type: 'SET_COMPANY', payload: location.pathname.slice(1) })
-    else dispatch({ type: 'SET_COMPANY', payload: '' })
-
-    dispatch({ type: 'SET_FILTERED_RESULTS' })
-  }, [location.pathname])
+const Home = () => {
+  const { state } = useContext()
 
   return (
     <Container>
@@ -97,4 +49,4 @@ Home.propTypes = {
   }).isRequired,
 }
 
-export default Home
+export default withFilters(Home)
